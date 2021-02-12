0930201ce0cfb4e10032cdc7bdc12f36c63e18a5
Jasper Zonneveld <jasper@swis.nl>
2021-02-12 15:52:13 +0100

    Import facades instead of aliases

--- src/Gwnobots/LaravelHead/LaravelHead.php
+++ src/Gwnobots/LaravelHead/LaravelHead.php
@@ -1,9 +1,9 @@
 <?php namespace Gwnobots\LaravelHead;
 
-use App;
-use Config;
-use File;
-use URL;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\URL;
 
 class LaravelHead {
 
